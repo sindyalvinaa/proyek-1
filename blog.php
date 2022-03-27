4fafea5eeebf912ccdf86c6b1f5b3e6417e2ffc7
= <=>
2022-03-27 20:55:19 +0700

    Blog

---
+++ blog.php
@@ -0,0 +1,205 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags --> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <div class = "blog">
+    <div class="container">
+        <div class="judul text-center mt-5">
+        <h1 class = "display-4"><span class = "font-weight-bold"> Rita Fashion Our Blog </span></h1><hr>
+          <p class="lead font-weight-bold"> Our Awesome News <br></p>
+          <img src="images/SKIRT-ZASKIA-SILVER.jpg"><br><br>
+          <h3 class="font-weight-bold">Rita Fashion</h3>
+          <h5>Rita Fashion adalah pusat hijab jogja terlengkap dan termurah dengan kualitas yang baik. 
+          Rita Fashion memiliki banyak variant seperti pashmina, khimar, hijab instan, hijab segi empat (square)
+             dan banyak variant lain yang menarik.</h5><br>
+          <h3 class="font-weight-bold">Tren Fashion ditahun 2022</h3><hr>
+          <h5>Bagi orang-orang yang memiliki minat pada fashion tentu sangat penasaran dengan trend pakaian 2022 bukan?
+             Apakah akan ada trend baru dalam busana muslim wanita? Atau justru trend-trend lama akan kembali bangkit 
+             dan menjadi salah satu bagian dari trend hijab di tahun 2022 ini? Meski fashion memang terus berputar, 
+             tetapi tak ada salahnya jika kita mengintip prediksi trend fashion hijab untuk tahun 2022. FYI, berbagai 
+             trend model hijab 2022 sudah diprediksi oleh sejumlah fashion desainer Tanah Air hingga luar negeri yang 
+             pastinya membuat kamu ingin segera mengoleksinya. Kira-kira seperti apa trend hijab 2022? Berikut ulasannya!</h5><br>
+          <h3 class="font-weight-bold">Trend Hijab 2021</h3><hr><br>
+          <h4 class="font-weight-bold">1. Hijab pashmina</h4>
+          <h5>Pertama kali populer di tahun 2010, model hijab pashmina diprediksi akan kembali digemari oleh para hijabers. 
+            Hijab pashmina 2022 akan hadir dengan pilihan warna yang lebih cerah dengan menonjolkan sentuhan yang feminim. 
+            Selain warna pashmina, pashmina instan dengan bahan yang nyaman juga mulai digemari oleh para hijabers.</h5><br>
+          <h4 class="font-weight-bold">2. Hijab segi empat</h4>
+          <h5>Karena masa pandemi yang mengharuskan masyarakat lebih banyak di rumah membuat model hijab yang praktis lebih 
+            banyak diminati. Oleh karena itu, selain pashmina, hijab segi empat juga akan menjadi populer di tahun 2021. 
+            Jilbab segiempat bahan katun voal diprediksi akan ramai diburu hijabers karena memang mudah dibentuk dan nyaman 
+            digunakan. Bahkan hijab segiempat 2022 akan hadir dengan warna soft, natural, cutting lose dan motif-motif pilihan 
+            yang cantik dan minimalis untuk dikenakan di segala suasana.</h5><br>
+          <h4 class="font-weight-bold">3. Busana puff sleeves</h4>
+          <img src="images/Busana-puff-sleeves.jpg">
+          <h5>Trend busana hijab 2022 berikutnya adalah busana puff sleeves. Sesuai dengan namanya, busana puff sleeves ini 
+            memiliki detail lengan yang menggembung pada kanan kirinya. Meski bukan fashion baru, namun puff sleeves menjadi 
+            salah satu fashion item yang belakangan ini banyak digandrungi oleh para hijabers. Busana puff sleeves ini mampu 
+            memberikan nuansa yang chic dan feminim pada penampilan kamu. Bahkan model busana satu ini bisa dengan mudah 
+            dipadukan dengan berbagai fashion hijab lainnya.</h5><br>
+          <h4 class="font-weight-bold">4. Vest Korea</h4>
+          <img src="images/Vest-Korea.jpg">
+          <h5>Terinspirasi dari trend fashion Korea, Vest Korea menjadi salah satu trend hijab 2021 di Indonesia. Vest Korea 
+            sendiri berbentuk seperti tanktop atau busana tanpa lengan yang dijadikan luaran untuk mempermanis outfit. 
+            Item fashion ini bahkan sudah banyak digunakan oleh selebgram hijab tak hanya di Indonesia tapi juga di luar negeri. 
+            Penggunaan vest Korea ini memang mampu membuat tampilan semakin modis. Cara padupadannyapun sangat mudah, kamu bisa 
+            mengombinasikan vest Korea dengan dress, blus hingga kemeja sesuai style fashion yang kamu inginkan.</h5><br>
+          <h4 class="font-weight-bold">5. Sleepwear Piyama</h4>
+          <img src="images/Sleepwear-Piyama.jpg">
+          <h5>Tak ada bedanya dengan trend fashion 2020, sleepwear piyama juga akan menjadi trend hijab 2021. Dengan perkembangan 
+            fashion, sleepwear piyama kini tidak hanya dipakai saat tidur tapi kamu pun bisa menggunakannya untuk berpergian agar 
+            penampilan makin terlihat stunning. Kamu bisa memilih piyama set dengan motif kekinian seperti motif shibori hingga 
+            tie dye yang memang sangat digemari belakangan ini. Juga bahan rayon set yang mengkilap sehingga terlihat lebih chic 
+            namun juga memberikan kenyamanan sepanjang waktu.</h5><br><h4 class="font-weight-bold">6. Busana muslim dengan tropical vibes</h4>
+          <h5>Apapun model dan jenisnya, busana muslim 2021 diprediksi akan diramaikan dengan busana muslim dengan nuansa tropical 
+            yang cocok untuk cuaca panas di Indonesia. Bukan hanya bahannya saja yang menyesuaikan dengan iklim Indonesia, namun 
+            busana muslim dengan tropical vibes ini akan hadir dengan warna cerah dan motif-motif yang identik dengan nuansa tropis 
+            seperti motif flamingo atau floral yang memang menjadi salah satu trend motif 2021.</h5><br>
+          <h4 class="font-weight-bold">7. Busana warna vibrant/cerah</h4>
+          <img src="images/Busana-warna-vibrant.jpg">
+          <h5>Selain sentuhan tropical, trend warna pada hijab juga diprediksi akan mengalami perubahan. Jika tahun lalu, warna-warna 
+            pastel lebih mendominasi, maka tren hijab pada tahun 2021 akan mengedepankan warna vibrant yang lebih berani. Busana warna 
+            cerah ini akan memberikan kesegaran pada penampilan kamu dengan perpaduan warna yang memiliki energi kegembiraan, seperti 
+            turquoise, kuning, hingga pink dengan nuansa yang soft.</h5><br>
+          <h4 class="font-weight-bold">8. Busana warna lilac</h4>
+          <img src="images/Busana-warna-lilac.jpg">
+          <h5>Masih berhubungan dengan warna vibrant, warna lilac diprediksi akan menjadi salah satu warna dalam dunia fashion hijab 
+            yang paling digemari oleh para hijabers. FYI, warna lilac sendiri merupakan warna ungu muda dengan sentuhan lembut. 
+            Warna lilac biasanya identik dengan tampilan yang feminin dan juga berkaratker dan memberiikan kesan estetik hingga 
+            mempermanis outfit. Tak hanya warna lilac, lavender dan moss juga diprediksi akan mendominasi trend fashion muslim 2021.</h5><br>
+          <h4 class="font-weight-bold">9. Busana muslim bernuansa earthy tone</h4>
+          <img src="images/Busana-muslim-bernuansa-earthy-tone.jpg">
+          <h5>Meski warna vibrant dan lilac mendominasi 2021, bukan berarti nuansa earthy tone tak lagi diminati. Justru busana muslim 
+            dengan nuansa earthy tone akan meramaikan model hijab syari. Berbagai model busana muslim dari hijab hingga gamis akan hadir 
+            dengan beragam warna bernuansa earthy tone seperti warna terracotta, kuning, biru muda, dan olive. Warna-warna bernuansa earthy 
+            tone akan memberikan nuansa calm dan elegan pada penampilan kamu.</h5><br>
+          <h4 class="font-weight-bold">10. Hijab berbahan katun dan voal</h4>
+          <h5>Sementara untuk bahan hijab, katun tetap jadi andalan karena tingkat kenyamanannya. Mengingat masih pandemi dan kebanyakan 
+            wanita muslim mencari busana yang nyaman dan praktis, jadi nggak heran jika hijab berbahan katun menjadi trend di tahun 2021.
+            Selain katun, hijab voal juga menjadi salah satu bahan hijab yang digemari belakangan ini. Bahan hijab voal adalah kain voille 
+            yang memiliki beragam warna dan motif. Sifatnya semi transparan, tipis tetapi jika dilipat tidak akan menerawang. Kain voille 
+            ini kebanyakan terbuat dari 100% katun. Akan tetapi, ada juga variasi kain voille yang terbuat dari kombinasi serat rayon, linen,
+             silk, dan polyester.</h5><br>
+          <h4 class="font-weight-bold">11. Windbreaker jacket dengan nuansa colorful</h4>
+          <img src="images/Windbreaker-jacket-dengan-nuansa-colorful.jpg">
+          <h5>Di Indonesia sendiri, jaket sudah menjadi outfit wajib yang digunakan saat keluar rumah. Jadi nggak heran, setiap tahunnya ada 
+            saja desain jaket terbaru yang meramaikan trend fashion 2021. Untuk para hijabers, salah satu jaket yang menjadi tren adalah 
+            windbreaker jacket. Outer bergaya sporty ini menjadi incaran seiring dengan naiknya tren olahraga untuk menjaga kebugaran selama 
+            pandemi. Windbreaker jacket pada umumnya terkesan boyish dengan warna-warna netral, namun kini windbreaker jacket hadir dengan 
+            nuansa colorful. Banyak windbreaker jacket yang memiiliki motif cantik sehingga membuat hijabers tetap terlihat feminin dan nyaman.</h5><br>
+          <h4 class="font-weight-bold">12. Bucket hat hijab</h4>
+          <img src="images/Bucket-hat-hijab.jpg">
+          <h5>Bucket hat memang menjadi trend aksesoris hijab yang 2 tahun belakangan ini paling digemari, begitu juga dengan tahun 2021 
+            yang menunjukkan kalau bucket hat masih akan ramai digunakan oleh para fashion enthusiast. Bahkan sudah banyak selebgram hijabers 
+            yang menggunakan bucket hat dengan model kekinian untuk tren fashion hijab 2021.</h5><br>
+          <h4 class="font-weight-bold">13. Konsep minimalis</h4>
+          <img src="images/Konsep-minimalis.jpg">
+          <h5>Pandemi yang terjadi sepanjang 2020 juga sangat berpengaruh pada perubahan trend fashion. Menjalani masa-masa sulit dari tahun 2020, 
+            membuat kita membutuhkan sesuatu yang menenangkan untuk digunakan. Jadi nggak heran kalau busana muslim dengan konsep minimalis 
+            diprediksi akan banyak diburu oleh para hijabers. Dengan garis potongan yang sederhana dan rapi serta pengaplikasian warna-warna 
+            monochrome, kamu akan tampil lebih fresh dan lebih kekinian.</h5><br>
+          <h4 class="font-weight-bold">14. Aksesoris yang minim namun playful</h4>
+          <img src="images/Aksesoris-yang-playful.jpg">
+          <h5>Mengimbangi trend fashion hijab bernuansa minimalis, ada baiknya memadupadankannya dengan aksesoris yang mampu membuat penampilan 
+            kita menjadi playful dan tidak monoton. Tak perlu aksesori yang terlalu heboh dan ramai, kamu bisa cukup menambahkan beberapa aksesoris 
+            hijab yang kamu miliki atau tas mini untuk membuat penampilanmu menjadi lebih chic dan menarik.</h5><br>
+          <h4 class="font-weight-bold">15. Kemeja kerah cuban</h4>
+          <img src="images/Kemeja-kerah-cuban.jpg">
+          <h5>Selain busana puff sleeve, kemeja dengan kerah cuban juga menjadi salah satu trend hijab 2021 yang wajib kamu miliki. Kemeja kerah cuban 
+            memiliki desain daun kerah yang tidak menyelimuti leher. Jadi model ini masih termasuk pilihan kerah kemeja yang cocok untuk digunakan pada 
+            acara kasual. Kemeja yang bernuansa kasual ini cenderung memiliki leher yang agak rendah, sehingga kamu bisa menggunakan manset sebelum menggunakan kemeja.
+            Itulah beberapa trend hijab yang diprediksi akan banyak muncul di tahun 2021. Secara garis besar trend hijab 2021 lebih menonjolkan konsep minimalis 
+            dengan unsur kesederhanaan namun tetap menarik dan fresh. So, buruan koleksi fashion item hijab yang akan menjadi trend 2021 agar penampilan kamu 
+            senantiasa stylish dan kekinian</h5>
+            <img src="images/SQUARE-POLLYCOTTON-SOFTPINK.jpg"><br><br>
+            <h3 class="font-weight-bold">Tips Memilih Warna Hijab Sesuai Warna Kulit</h3>
+            <h5>Berhijab memang sangat mempengaruhi penampilan apalagi kalau kamu sedang memilih warna yang cocok dengan warna kulit 
+              tidak mau asal pakai kan ini dia tips memilih warna hijab sesuai warna kulit kamu</h5><br>
+          <h4 class="font-weight-bold">Warna Kulit Putih</h4>
+          <h5>Pemilihan untuk warna putih sebenarnya masuk dengan warna apa saja namun hindari warna tertentu seperti soft itu akan 
+            membuat kulit kamu nampak pucat gunakan warna-warna tegas dan gelap seperti hitam, biru tua, merah marun, atau ungu.</h5><br>
+            <h4 class="font-weight-bold">Warna Kulit Kuning Langsat</h4>
+            <h5>Umumnya wanita indonesia berkulit kuning langsat kuning langsat sebenarnya netral untuk pemilihan warna hijab 
+            gunakan warna-warna cerah seperti merah, biru, kuning, dan pink terang agar kulit terlihat lebih cerah dan segar.</h5><br>
+            <h4 class="font-weight-bold">Warna Kulit Sawo Matang</h4>
+            <h5>Kulit sawo matang harus lebih teliti dalam memilih warna agar tidak salah, pilihlah warna seperti warna pastel
+             dan warna-warna yang cenderung gelap, seperti navy, merah marun, ungu gelap, dan hitam.</h5><br>
+            <h4 class="font-weight-bold">Warna Kulit Gelap</h4>
+          <h5>Biasanya pemilik kulit warna gelap hampir tidak percaya diri dalam pemilihan warna kalau kamu memakai warna gelap
+             akan menambah kesan semakin gelap dan kusam, pilihlah warna lembut seperti warna pastel, lavender, baby blue, 
+             pink tua, pink, kuning kunyit, cokelat muda, atau merah marun.</h5>
+            <br></div></div><br><br>
+
+  <div class="row">
+  <div class="container">
+        <div class="row footer-body">
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-5" class="widget widget_nav_menu"><h3 class="widget-title">Information</h3><div 
+          class="menu-information-container"><ul id="menu-information" 
+          class="menu"><li id="menu-item-3141" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3141">
+            <a href="about_us.php">About Us</a></li>
+          <li id="menu-item-3142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3142">
+            <a href="contact.php">Contact Us</a></li>
+          <li id="menu-item-3143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3143">
+            <a href="ThePrivacyPolicy.php">The Privacy Policy</a></li>
+          <li id="menu-item-3144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3144">
+            <a href="FAQ.php">FAQ</a></li></ul></div></aside></section>
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-2" class="widget widget_nav_menu"><h3 class="widget-title">Collections</h3><div 
+          class="menu-main-navigation-container"><ul id="menu-main-navigation-1" class="menu"><li id="menu-item-1328" 
+          class="menu-item menu-item-type-post_type_archive menu-item-object-product menu-item-1328">
+          <a href="produk_pembeli">Product</a></li>
+          <li id="menu-item-1698" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1698">
+            <a href="order_pembeli.php">Add Cart</a></li>
+          <li id="menu-item-1700" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1700">
+            <a href="blog.php">Blog</a></li>
+          <li id="menu-item-1701" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1701">
+            <a href="contact.php">Contact</a></li></ul></div></aside></section>
+
+  <!-- Footer -->
+    <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+          <div class="copyright">  <br><br>
+            <strong>Copyright</strong> <i class="far fa-copyright"></i> 2022 -  Designed by RESITA </p>
+          </div>
+        </div>
+  <!-- Footer -->
+
+  <!-- Header -->
+  <div class="medsos">
+        <div class="container">
+            <ul>  <br><br>
+            <label class="font-weight-bold"> Follow Us </label>
+              <li><a href="https://www.facebook.com/fashionjamanow/"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://instagram.com/ritafashion.id?utm_medium=copy_link"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" 
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script></body></html><?php ?>
