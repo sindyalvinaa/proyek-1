121715c9915ad0d4b35e79a85b432d11df533a13
= <=>
2022-03-27 21:21:53 +0700

    Privacy Policy

---
+++ ThePrivacyPolicy.php
@@ -0,0 +1,137 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <!-- Required meta tags --> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <div class = "ThePrivacyPolicy">
+    <div class="container">
+        <div class="judul text-center mt-5">
+          <img src="images/background/background_3.jpg">
+            <hr>
+              <h5>Rita Fashion The Privacy Policy<h5>
+              <h1 class = "display-4"><span class = "font-weight-bold"> The Privacy Policy </span></h1>
+            <hr>
+            <br>
+            <h4 class="font-weight-bold">DISCLAIMER</h4>
+            <h6>Locavore four dollar toast hoodie Schlitz iPhone, twee roof party leggings Blue Bottle American Apparel salvia 
+                pickled. Semiotics gluten-free Helvetica Truffaut disrupt twee readymade, next level umami kogi Brooklyn artisan 
+                Odd Future meggings plaid. Post-ironic Schlitz asymmetrical butcher Williamsburg.
+
+                Irony hella letterpress pour-over. Flannel shabby chic lumbersexual, organic asymmetrical Schlitz lo-fi yr seitan 
+                health goth put a bird on it taxidermy sustainable. Stumptown Bushwick ethical 8-bit jean shorts Godard typewriter, 
+                Pinterest PBR&B whatever single-origin coffee Echo Park chia Odd Future. Taxidermy sustainable butcher fingerstache, 
+                cornhole pug craft beer PBR&B stumptown Pinterest chia PBR sriracha cronut. Austin McSweeneys ugh, fanny pack butcher 
+                DIY normcore lumbersexual semiotics jean shorts ethical cornhole tilde narwhal.</h6>
+            <br>
+            <h4 class="font-weight-bold">MEANS OF OBTAINING INFORMATION</h4>
+            <h6>Cookies
+                Fingerstache occupy keffiyeh, normcore raw denim health goth Brooklyn lo-fi Echo Park cardigan pop-up sriracha 8-bit. 
+                Gastropub 3 wolf moon 8-bit butcher. Gentrify roof party umami Schlitz plaid, fixie American Apparel +1 banh mi keffiyeh 
+                crucifix readymade disrupt twee cray. Pitchfork quinoa cliche single-origin coffee. Polaroid tote bag Schlitz drinking 
+                vinegar twee, small batch vinyl paleo plaid wolf pork belly brunch. Vinyl pork belly mixtape Vice farm-to-table, seitan 
+                direct trade kogi pug literally ethical sustainable viral.
+                Site Statistics
+                Semiotics McSweeneys next level put a bird on it Pitchfork. Tumblr lomo semiotics, dreamcatcher food truck organic Schlitz 
+                chambray normcore meh tousled.
+                Age Requirement
+                Fingerstache occupy keffiyeh, normcore raw denim health goth Brooklyn lo-fi Echo Park cardigan pop-up sriracha 8-bit. 
+                Gastropub 3 wolf moon 8-bit butcher. Gentrify roof party umami Schlitz plaid, fixie American Apparel +1 banh mi keffiyeh 
+                crucifix readymade disrupt twee cray. Pitchfork quinoa cliche single-origin coffee. Polaroid tote bag Schlitz drinking 
+                vinegar twee, small batch vinyl paleo plaid wolf pork belly brunch. Vinyl pork belly mixtape Vice farm-to-table, seitan 
+                direct trade kogi pug literally ethical sustainable viral.</h6>
+            <br>
+            <h4 class="font-weight-bold">MODIFICATION OF POLICIES</h4>
+            <h6> Fingerstache occupy keffiyeh, normcore raw denim health goth Brooklyn lo-fi Echo Park cardigan pop-up sriracha 8-bit. 
+               Gastropub 3 wolf moon 8-bit butcher. Gentrify roof party umami Schlitz plaid, fixie American Apparel +1 banh mi keffiyeh 
+               crucifix readymade disrupt twee cray. Pitchfork quinoa cliche single-origin coffee. Polaroid tote bag Schlitz drinking 
+               vinegar twee, small batch vinyl paleo plaid wolf pork belly brunch. Vinyl pork belly mixtape Vice farm-to-table, seitan 
+               direct trade kogi pug literally ethical sustainable viral.
+                Semiotics McSweeneys next level put a bird on it Pitchfork. Tumblr lomo semiotics, dreamcatcher food truck organic 
+                Schlitz chambray normcore meh tousled.</h6><br></div></div><br><br><br><br>
+                
+  <div class="row">
+  <div class="container">
+        <div class="row footer-body">
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-5" class="widget widget_nav_menu">
+            <h3 class="widget-title">Information</h3>
+            <div class="menu-information-container">
+              <ul id="menu-information" class="menu">
+                <li id="menu-item-3141" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3141">
+            <a href="about_us.php">About Us</a></li>
+          <li id="menu-item-3142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3142">
+            <a href="contact.php">Contact Us</a></li>
+          <li id="menu-item-3143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3143">
+            <a href="ThePrivacyPolicy.php">The Privacy Policy</a></li>
+          <li id="menu-item-3144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3144">
+            <a href="FAQ.php">FAQ</a></li>
+          </ul></div></aside></section>
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-2" class="widget widget_nav_menu">
+            <h3 class="widget-title">Collections</h3>
+            <div class="menu-main-navigation-container">
+              <ul id="menu-main-navigation-1" class="menu">
+                <li id="menu-item-1328" class="menu-item menu-item-type-post_type_archive menu-item-object-product menu-item-1328">
+          <a href="produk_pembeli.php">Product</a></li>
+          <li id="menu-item-1698" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1698">
+            <a href="order_pembeli.php">Add Cart</a></li>
+          <li id="menu-item-1700" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1700">
+            <a href="blog.php">Blog</a></li>
+          <li id="menu-item-1701" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1701">
+            <a href="contact.php">Contact</a></li>
+          </ul></div></aside> </section>
+
+
+  <!-- Footer -->
+    <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+          <div class="copyright">
+            <br><br>
+            <strong>Copyright</strong> <i class="far fa-copyright"></i> 2022 -  Designed by RESITA </p>
+          </div>
+        </div>
+  <!-- Footer -->
+
+  <!-- Header -->
+  <div class="medsos">
+        <div class="container">
+            <ul>
+              <br><br>
+            <label class="font-weight-bold"> Follow Us </label>
+              <li><a href="https://www.facebook.com/fashionjamanow/"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="https://instagram.com/ritafashion.id?utm_medium=copy_link"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+  <!-- Header -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" 
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" 
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" 
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script></body></html><?php ?>
