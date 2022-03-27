8ba17c760fa106e69904ce0276e142098184b413
= <=>
2022-03-27 20:43:44 +0700

    About Us

---
+++ about_us.php
@@ -0,0 +1,127 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags --> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <div class = "about_us">
+    <div class="container">
+        <div class="judul text-center mt-5">
+          <img src="images/background/background_3.jpg">
+        <div class="vc_empty_space"   style="height: 30px"><span class="vc_empty_space_inner"></span></div>
+	    <div  class="wpb_single_image wpb_content_element vc_align_left">
+        <h2 style="font-size: 44px;color: #000000;line-height: 44px;text-align: center" class="vc_custom_heading" >MEET OUR TEAM</h2>
+        <h5 style="font-size: 20px;color: #242d00;line-height: 24px;text-align: center" class="vc_custom_heading" >THE RIGHT PEOPLE IN THE RIGHT PLACE AND IN THE RIGHT TIME</h5>
+        <div class="vc_empty_space"   style="height: 40px"><span class="vc_empty_space_inner"></span></div></div></div></div></div>
+          <div class="vc_row wpb_row vc_row-fluid">
+            <div class="wpb_column vc_column_container vc_col-sm-2"><div class="vc_column-inner vc_custom_1485270531028">
+              <div class="wpb_wrapper"></div></div></div>
+            <div class="wpb_column vc_column_container vc_col-sm-8"><div class="vc_column-inner vc_custom_1485270540813">
+          <div class="wpb_wrapper"><div class="vc_row wpb_row vc_inner vc_row-fluid">
+        <div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
+        <h2 style="font-size: 24px;color: #000000;line-height: 26px;text-align: center" class="vc_custom_heading" >AHMAD NADHIL, S.E.</h2>
+          <h5 style="font-size: 16px;color: #242d00;line-height: 18px;text-align: center" class="vc_custom_heading" >CEO & Co-Founder</h5></div></div></div>
+            <div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
+        <h2 style="font-size: 24px;color: #000000;line-height: 26px;text-align: center" class="vc_custom_heading" >TANJUNG MULIA D.N., S.Psi.</h2>
+          <h5 style="font-size: 16px;color: #242d00;line-height: 18px;text-align: center" class="vc_custom_heading" >CREATIVE DIRECTOR & Co-Founder</h5></div></div></div>
+            <div class="wpb_column vc_column_container vc_col-sm-4"><div class="vc_column-inner"><div class="wpb_wrapper">
+          <h2 style="font-size: 24px;color: #000000;line-height: 26px;text-align: center" class="vc_custom_heading" >ADIB IBNU YASA, M.B.A.</h2>
+            <h5 style="font-size: 16px;color: #242d00;line-height: 18px;text-align: center" class="vc_custom_heading" >BUSINESS DEVELOPMENT MANAGER</h5></div></div></div></div></div></div></div>
+          <div class="wpb_column vc_column_container vc_col-sm-2"><div class="vc_column-inner vc_custom_1485270549758"><br><br>
+
+
+   <img src="images/Square-Voal.jpg"><br><br>
+      <h4 style="font-size: 30px;color: #000000;line-height: 34px;text-align: left;font-family:Karla;font-weight:400;font-style:normal" 
+           class="vc_custom_heading vc_custom_1582969959612" >ASSALAMUALAIKUM SAHABAT RITA FASHION</h4>
+	<div class="wpb_text_column wpb_content_element " >
+		<div class="wpb_wrapper">
+			<p style="font-size: 18px;">Rita Fashion merupakan pusat grosir pakaian untuk segala kalangan di Indonesia. Rita Fashion 
+            mempunyai ciri khas pakaian yang tetap penuh gaya dan kekinian, dan dengan harga terjangkau namun dengan 
+            kualitas yang mumpuni.. Rita Fashion hadir pertama kali pada Januari tahun 2016. Rita Fashion hadir dari 
+            berjualan pakaian online kecil-kecilan, hingga pada akhirnya memiliki toko offline maupun online yang selalu ramai oleh pembeli. 
+            Rita Fashiom yakin bahwa setiap orang dapat tampil trendy dan kekinian.</p>
+    <img src="images/SQUARE-SHERINA-BLACK-scaled.jpg"><br><br>
+      <h4 style="font-size: 30px;color: #000000;line-height: 34px;text-align: left;font-family:Karla;font-weight:400;font-style:normal" 
+        class="vc_custom_heading vc_custom_1582969959612" >PERJALANAN KAMI.....</h4>
+    <div class="wpb_text_column wpb_content_element " >
+      <div class="wpb_wrapper">
+        <p style="font-size: 18px;">Rita Fashion pertama kali muncul di Parengan, tepatnya di Jl Raya Sendangrejo-Jatirogo,
+              Kec. Parengan, Kab. Bojonegoro, Jawa Timur dan juga melalui akun Instagram @rita_fashion. Kini, selama hampir 
+              6 tahun kehadirannya. Rita Fashion telah melayani ribuan konsumen, mulai dari sabang sampai marauke. Selain itu, 
+              Rita Fashion juga telah memiliki 1 offline store di kota Parengan. Rita Fashion juga telah mampu memberdayakan lebih 
+              dari 90 penjahit yang berlokasi di tiga titik di Provinsi Jawa Timur. Rita Fashion selalu berusaha untuk dapat menjadi 
+              sentra pakaian kekinian, termurah dan terlengkap se-Indonesia.</p>                                                          
+
+    <div class="row">
+    <div class="container">
+        <div class="row footer-body">
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-5" class="widget widget_nav_menu"><br>
+            <h3 class="widget-title">Information</h3>
+            <div class="menu-information-container">
+              <ul id="menu-information" class="menu">
+                <li id="menu-item-3141" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3141">
+            <a href="about_us.php">About Us</a></li>
+          <li id="menu-item-3142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3142">
+            <a href="contact.php">Contact Us</a></li>
+          <li id="menu-item-3143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3143">
+            <a href="ThePrivacyPolicy.php">The Privacy Policy</a></li>
+          <li id="menu-item-3144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3144">
+            <a href="FAQ.php">FAQ</a></li></ul></div></aside></section>
+          <section class="large-3 medium-4 columns column-widget right-align"><br>
+          <aside id="nav_menu-2" class="widget widget_nav_menu"><h3 class="widget-title">Collections</h3><div 
+          class="menu-main-navigation-container"><ul id="menu-main-navigation-1" class="menu"><li id="menu-item-1328" 
+          class="menu-item menu-item-type-post_type_archive menu-item-object-product menu-item-1328">
+          <a href="produk_pembeli.php">Product</a></li>
+          <li id="menu-item-1698" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1698">
+            <a href="order_pembeli.php">Add Cart</a></li>
+          <li id="menu-item-1700" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1700">
+            <a href="blog.php">Blog</a></li>
+          <li id="menu-item-1701" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1701">
+            <a href="contact.php">Contact</a></li></ul></div></aside> </section>
+
+  <!-- Footer -->
+    <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6"><br>
+            <div class="copyright">
+            <strong>Copyright</strong> <i class="far fa-copyright"></i> 2022 -  Designed by RESITA </p>
+          </div>
+        </div>
+  <!-- Footer -->
+
+  <!-- Header -->
+  <div class="medsos">
+        <div class="container">
+            <ul><br>
+            <label class="font-weight-bold"> Follow Us </label>
+            <li><a href="https://www.facebook.com/fashionjamanow/"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="https://instagram.com/ritafashion.id?utm_medium=copy_link"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" 
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script></body></html><?php ?>
