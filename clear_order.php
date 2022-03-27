c1f6c37f2ae224ec44440522029318bbeec1c934
= <=>
2022-03-27 20:58:14 +0700

    Clear Order

---
+++ clear_order.php
@@ -0,0 +1,9 @@
+<?php 
+include('koneksi.php');
+$id = $_GET['id'];
+$hapus= mysqli_query($koneksi, "DELETE FROM pemesanan WHERE id_pemesanan='$id'");
+if($hapus)
+	header('location: order.php');
+else
+	echo "Hapus data gagal";
+ ?>
