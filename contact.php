85427426ce87ef2b882b36341050c7418ffc9454
= <=>
2022-03-27 21:04:44 +0700

    Contact

---
+++ contact.php
@@ -0,0 +1,131 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags --> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <div class = "contact">
+    <div class="container">
+        <div class="judul text-center mt-5">
+        <img src="images/background/background_3.jpg"><br><br><br>
+        <h3 class = "display-5"><span class = "font-weight-bold">Grand Opening Rita Fashion Store 2 </span></h3><hr><br><br>
+    <div class="wpb_video_wrapper"><div class="video-container"><iframe title="Grand Opening Rita Fashion Store 2" 
+    width="1000" height="600" src="https://www.youtube.com/embed/jVcqWVzRdVA?feature=oembed" frameborder="0" 
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div></div><br><br>
+<div class="vc_separator wpb_content_element vc_separator_align_left vc_sep_width_100 vc_sep_border_width_3 vc_sep_pos_align_left
+ vc_custom_1487757377709 vc_separator-has-text" ><span class="vc_sep_holder vc_sep_holder_l"><span  style="border-color:#a8e8e2;" 
+ class="vc_sep_line"></span></span><h4>OUR ADDRESS</h4><span class="vc_sep_holder vc_sep_holder_r"><span
+ style="border-color:#a8e8e2;" class="vc_sep_line"></span></span>
+	<div class="wpb_text_column wpb_content_element  wpb_animate_when_almost_visible wpb_fadeInRight fadeInRight" >
+		<div class="wpb_wrapper">
+	<h5>Parengan</h5>
+    <p>0858-5654-3339<br/>
+    Jl Raya Sendangrejo-Jatirogo, Krajan, RT.07/RW.04, Kec. Parengan, Kab. Tuban, Jawa Timur 62366</p></div></div>
+    <div class="wpb_raw_code wpb_content_element wpb_raw_html" >
+		<div class="wpb_wrapper">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.2050856525952!2d110.39620151453845!3d
+                -7.7680639944015155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59bc3d809405%3A0x56cf22e49057e
+                379!2sNadiraa%20Hijab!5e0!3m2!1sid!2sid!4v1580200299603!5m2!1sid!2sid" width="800" height="400" frameborder="0" 
+                style="border:0;" allowfullscreen=""></iframe></div></div><br>
+<div class="vc_separator wpb_content_element vc_separator_align_left vc_sep_width_100 vc_sep_border_width_3 vc_sep_pos_align_left
+ vc_custom_1487757377709 vc_separator-has-text" ><span class="vc_sep_holder vc_sep_holder_l"><span  style="border-color:#a8e8e2;" 
+ class="vc_sep_line"></span></span><hr><br><h4>OPENING HOURS</h4><span class="vc_sep_holder vc_sep_holder_r"><span
+ style="border-color:#a8e8e2;" class="vc_sep_line"></span></span></div>
+	<div class="wpb_text_column wpb_content_element " >
+		<div class="wpb_wrapper">
+			<p><strong>All day</strong>: 9am to 10pm</p></div></div>
+        <div class="shortcode_icon_box icon_position_top icon_style_normal with_separator">
+			<div class="icon_wrapper" style="background-color:#ffffff; border-color:#dd6c6c">
+				<div class="icon basic-paperplane" style="color:#dd6c6c"></div>
+			</div><br><h4 class="icon_box_title">Admin Luar Kota</h4><a class="icon_box_read_more" 
+            href="https://api.whatsapp.com/send?phone=6282221547323&amp;text=Hallo%20kak%20admin%20nadiraahijab.id%20hijab%20saya%20mau%20order">Whatsapp</a></div>
+	</div></div></div><div class="wpb_column vc_column_container vc_col-sm-6"><div class="vc_column-inner"><div class="wpb_wrapper">		
+		<div class="shortcode_icon_box icon_position_top icon_style_normal with_separator">
+			<div class="icon_wrapper" style="background-color:#ffffff; border-color:#dd6c6c">
+				<div class="icon basic-paperplane" style="color:#dd6c6c"></div><br>
+                <div class="container">
+        <div class="row footer-body">
+			</div><h4 class="icon_box_title">Admin Jawa Timur</h4><a class="icon_box_read_more" 
+            href="https://api.whatsapp.com/send?phone=6281215255138&amp;text=Hallo%20kak%20admin%20nadiraahijab.id%20hijab%20saya%20mau%20order">Whatsapps</a></div>
+	</div></div></div></div>		
+		<div class="shortcode_icon_box icon_position_top icon_style_normal with_separator">
+			<div class="icon_wrapper" style="background-color:#ffffff; border-color:#dd6c6c">
+				<div class="icon basic-paperplane" style="color:#dd6c6c"></div><br>
+                <div class="container">
+        <div class="row footer-body">
+			</div><h4 class="icon_box_title">Admin Jawa</h4><a class="icon_box_read_more" 
+            href="https://api.whatsapp.com/send?phone=6285711354835&amp;text=hallo%20kak%20nadiraahijab.id%20saya%20mau%20order">Whatsapps</a></div>
+	<div class="vc_empty_space"   style="height: 80px"><span class="vc_empty_space_inner"></span></div>
+    
+    <div class="row">
+    <div class="container">
+        <div class="row footer-body">
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-5" class="widget widget_nav_menu"><br>
+            <h3 class="widget-title">Information</h3>
+            <div class="menu-information-container">
+              <ul id="menu-information" class="menu">
+                <li id="menu-item-3141" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3141">
+            <a href="about_us.php">About Us</a></li>
+          <li id="menu-item-3142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3142">
+            <a href="contact.php">Contact Us</a></li>
+          <li id="menu-item-3143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3143">
+            <a href="ThePrivacyPolicy.php">The Privacy Policy</a></li>
+          <li id="menu-item-3144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3144">
+            <a href="FAQ.php">FAQ</a></li></ul></div></aside></section>
+          <section class="large-3 medium-4 columns column-widget right-align"><br>
+          <aside id="nav_menu-2" class="widget widget_nav_menu"><h3 class="widget-title">Collections</h3><div 
+          class="menu-main-navigation-container"><ul id="menu-main-navigation-1" class="menu"><li id="menu-item-1328" 
+          class="menu-item menu-item-type-post_type_archive menu-item-object-product menu-item-1328">
+          <a href="produk_pembeli.php">Product</a></li>
+          <li id="menu-item-1698" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1698">
+            <a href="order_pembeli.php">Add Cart</a></li>
+          <li id="menu-item-1700" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1700">
+            <a href="blog.php">Blog</a></li>
+          <li id="menu-item-1701" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1701">
+            <a href="contact.php">Contact</a></li></ul></div></aside> </section>
+
+  <!-- Footer -->
+      <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+          <div class="copyright"><br><br>
+            <strong>Copyright</strong> <i class="far fa-copyright"></i>2022 -  Designed by RESITA</p>
+          </div>
+          </div>
+  <!-- Footer -->
+
+    <!-- Header -->
+    <div class="medsos">
+        <div class="container">
+            <ul><br><br>
+            <label class="font-weight-bold"> Follow Us </label>
+              <li><a href="https://www.facebook.com/fashionjamanow/"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://instagram.com/ritafashion.id?utm_medium=copy_link"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" 
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script></body></html><?php ?>
