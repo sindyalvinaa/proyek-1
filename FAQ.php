f2d53cc3eaeb4f3614c29438734a85708d85b62f
= <=>
2022-03-27 21:13:27 +0700

    FAQ

---
+++ FAQ.php
@@ -0,0 +1,111 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags --> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <div class = "faq">
+    <div class="container">
+        <div class="judul text-center mt-5">
+        <img src="images/background/background_3.jpg"><hr>
+            <h5>FAQ<h5>
+        <h1 class = "display-4"><span class = "font-weight-bold"> Rita Fashion FAQ </span></h1>
+          <h5>Rita Fashion toko grosir pakaian murah berasal dari Jawa Timur, pakaian terlengkap dan terfavorit. 
+          Rita Fashion memiliki banyak variant pakaian murah & terlengkap<h5><br><br><br>
+        </div></div>
+
+<div class="row">
+	    <div class="large-12 columns"></div></div>
+<div id="1456128678198-158c6503-6b13" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Nadiraa punya toko offline ga?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Sobat Nadiraa bisa berkunjung ke toko kami beralamatkan di Jl. Selokan Mataram No.451, Pringwulung, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281</p>
+</div></div><div  class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Bagaimana cara tahu bahwa barang yang saya cari masih ada stoknya atau sudah habis?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Fitur belum tersedia jadi bisa telpon dulu ke nomer 0857-1135-4835</p>
+</div></div><div id="1456131834508-eeca16f7-9a15" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Apakah ada diskon khusus untuk berbelanja di website Nadiraa?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Diskon akan berlaku sesuai dengan ketentuan tertentu, oleh karena itu, stay tuned terus di ig Nadiraa yaa..</p>
+</div></div><div id="1456131874916-6d970df2-719b" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Produk Nadiraa ORI apa KW?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Produk kami adalah produk original produksi sendiri, selama anda berbelanja di channel &#8211; channel penjualan kami, kami pastikan produk yang ada terima adalah produk original</p>
+</div></div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_pos_align_center vc_separator_no_text vc_custom_1484816953339  vc_custom_1484816953339" ><span class="vc_sep_holder vc_sep_holder_l"><span  style="border-color:rgb(0,0,0);border-color:rgba(0,0,0,0.06);" class="vc_sep_line"></span></span><span class="vc_sep_holder vc_sep_holder_r"><span  style="border-color:rgb(0,0,0);border-color:rgba(0,0,0,0.06);" class="vc_sep_line"></span></span>
+</div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
+	<div class="wpb_text_column wpb_content_element  vc_custom_1583571364586" >
+		<div class="wpb_wrapper"><h2 class="shortcode_title secondary_font">Product Order</h2><p>&nbsp;</p></div></div>
+<div id="1458551898935-98576f0d-4942" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Tipe pembayaran apa saja yang disediakan oleh Nadiraa?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Transfer antar bank BRI, BCA &amp; Alfamart</p>
+</div></div><div id="1458551898561-ce22df3c-e3ea" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Pembayarannya bisa COD (Cash On Delivery, bayar ketika barang diterima) ?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Saat ini opsi pembayaran COD (saat barang diantar) belum tersedia</p>
+</div></div><div id="1458551898392-4557786f-8d31" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Kalau saya bayar dulu (bayar dimuka melalui transfer bank), saya takut ditipu.</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Saat ini opsi untuk DP belum tersedia, akan tetapi kami memberi jaminan bahwa anda tidak akan tertipu selama berbelanja di website kami. Anda juga bisa berbelanja melalui Shoppee</p>
+</div></div><div id="1458551898740-ec7850fb-636b" class="vc_toggle vc_toggle_simple vc_toggle_color_black  vc_toggle_size_md"><div class="vc_toggle_title"><h4>Foto-foto yang terpajang di situs Olstore.ID bagus. Apakah hasil editan ? Apakah barang yang saya beli nantinya seperti tampak di foto ?</h4><i class="vc_toggle_icon"></i></div><div class="vc_toggle_content"><p>Foto yang ditampilkan adalah foto yang dibuat khusus untuk menampilkan produk. Jika anda berada di Yogyakarta, anda dapat berkunjung ke toko kami untuk melihat produk secara langsung, atau hubungi customer service kami untuk informasi lebih lanjut</p>
+</div></div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="vc_separator wpb_content_element vc_separator_align_center vc_sep_width_100 vc_sep_pos_align_center vc_separator_no_text vc_custom_1484816962520  vc_custom_1484816962520" ><span class="vc_sep_holder vc_sep_holder_l"><span  style="border-color:rgb(0,0,0);border-color:rgba(0,0,0,0.06);" class="vc_sep_line"></span></span><span class="vc_sep_holder vc_sep_holder_r"><span  style="border-color:rgb(0,0,0);border-color:rgba(0,0,0,0.06);" class="vc_sep_line"></span></span>
+</div></div></div></div></div><div class="vc_row wpb_row vc_row-fluid"><div class="wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper">
+     <div class="woocommerce shortcode_products_slider">
+         <div id="products-carousel" class="products-carousels-1481400863">
+    <div class="product_thumbnail ">
+        <span class="button-loader"></span></div></div></div><br><br><br><br>
+
+  <div class="row">
+  <div class="container">
+        <div class="row footer-body">
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-5" class="widget widget_nav_menu">
+            <h3 class="widget-title">Information</h3>
+            <div class="menu-information-container">
+              <ul id="menu-information" class="menu">
+                <li id="menu-item-3141" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3141">
+            <a href="about_us.php">About Us</a></li>
+          <li id="menu-item-3142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3142">
+            <a href="contact.php">Contact Us</a></li>
+          <li id="menu-item-3143" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3143">
+            <a href="ThePrivacyPolicy.php">The Privacy Policy</a></li>
+          <li id="menu-item-3144" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3144">
+            <a href="FAQ.php">FAQ</a></li>
+          </ul></div></aside></section>
+          <section class="large-3 medium-4 columns column-widget right-align">
+          <aside id="nav_menu-2" class="widget widget_nav_menu"><h3 class="widget-title">Collections</h3><div 
+          class="menu-main-navigation-container"><ul id="menu-main-navigation-1" class="menu"><li id="menu-item-1328" 
+          class="menu-item menu-item-type-post_type_archive menu-item-object-product menu-item-1328">
+          <a href="produk_pembeli.php">Product</a></li>
+          <li id="menu-item-1698" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1698">
+            <a href="order_pembeli.php">Add Cart</a></li>
+          <li id="menu-item-1700" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1700">
+            <a href="blog.php">Blog</a></li>
+          <li id="menu-item-1701" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1701">
+            <a href="contact.php">Contact</a></li>
+          </ul></div></aside> </section>
+
+  <!-- Footer -->
+    <hr class="footer">
+      <div class="container">
+        <div class="row footer-body">
+          <div class="col-md-6">
+            <div class="copyright"><br><br>
+            <strong>Copyright</strong> <i class="far fa-copyright"></i> 2022 -  Designed by RESITA </p>
+          </div>
+        </div>
+  <!-- Footer -->
+
+  <!-- Header -->
+  <div class="medsos">
+        <div class="container">
+            <ul><br><br>
+            <label class="font-weight-bold"> Follow Us </label>
+              <li><a href="https://www.facebook.com/fashionjamanow/"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://instagram.com/ritafashion.id?utm_medium=copy_link"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" 
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script></body></html><?php ?>
