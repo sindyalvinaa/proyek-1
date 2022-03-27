4c451c862746d04be41bdd6e9008963ca9799794
RinaHariyanti <rinayanti1771@gmail.com>
2022-03-27 20:06:17 +0700

    Edit

---
+++ edit.php
@@ -0,0 +1,20 @@
+<?php 
+include('koneksi.php');
+$id_produk = $_POST['id_produk'];
+$nama_produk = $_POST['nama_produk'];
+$jenis_produk = $_POST['jenis_produk'];
+$stok = $_POST['stok'];
+$harga = $_POST['harga'];
+$detail_produk = $_POST['detail_produk'];
+$nama_file = $_FILES['gambar']['name'];
+$source = $_FILES['gambar']['tmp_name'];
+$folder = './upload/';
+
+move_uploaded_file($source, $folder.$nama_file);
+$edit = mysqli_query($koneksi, "UPDATE produk SET nama_produk='$nama_produk', jenis_produk='$jenis_produk', 
+stok='$stok', harga='$harga', detail_produk='$detail_produk', gambar='$nama_file' WHERE id_produk='$id_produk' ");
+if($edit)
+	header('location: daftar_produk.php');
+else
+	echo "Edit Produk Gagal";
+ ?>
